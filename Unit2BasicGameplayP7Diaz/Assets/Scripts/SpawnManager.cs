a47951d4f336492783c837719aaea7bbc94f80c3
Otrebor0417 <roborobert185@gmail.com>
2024-04-26 15:24:40 -0700

    Finished Game
    Finished Game

--- Unit2BasicGameplayP7Diaz/Assets/Scripts/SpawnManager.cs
+++ Unit2BasicGameplayP7Diaz/Assets/Scripts/SpawnManager.cs
@@ -6,20 +6,27 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
+    } 
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-
-            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20,20), 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        
+        
+        Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20, 20), 0, 20), animalPrefabs[animalIndex].transform.rotation);
     }
 }
